a382c84ab35977cdadf7e06274941705aa3625a3
혀느현스 <hyuns@hyuns.dev>
2022-03-24 20:30:14 +0900

    로고 & 파비콘 추가

--- src/screen/main.tsx
+++ src/screen/main.tsx
@@ -1,17 +1,40 @@
-import { Controller, Button } from "../components";
-import { Play, Pause, Cursor , CaretRight, CaretLeft, HandPointing, ArrowUpRight, Plus } from "phosphor-react";
+import { Controller, Button, Label } from "../components";
+import { Play, Cursor , CaretRight, CaretLeft, HandPointing, ArrowUpRight, Plus, Planet  } from "phosphor-react";
+import Logo from '../assets/lettering.png'
+import styled from "styled-components";
+
+const LogoDiv = styled.div`
+    position: fixed;
+    top: 32px;
+    left: 32px;
+    user-select: none;
+    -webkit-user-drag: none;
+
+    img {
+        height: 32px;
+        user-select: none;
+        -webkit-user-drag: none;
+    }
+`
 
 export default function Main() {
-
     return (
         <>
+            <LogoDiv>
+                <img src={Logo} alt='스페이스 그래비티 로고' />
+            </LogoDiv>
+            <Controller right={20} top={20}>
+                <Label>
+                    <Planet /> 123 | <Planet /> 123
+                </Label>
+            </Controller>
             <Controller left={20} bottom={20}>
                 <Button content={<Plus  />} tooltip='Pause' onClick={() => console.log('play')} />
                 <Button content={<Cursor />} tooltip='Play' onClick={() => console.log('play')} />
                 <Button content={<HandPointing  />} tooltip='Pause' onClick={() => console.log('play')} />
                 <Button content={<ArrowUpRight   />} tooltip='Pause' onClick={() => console.log('play')} />
             </Controller>
-            <Controller right={20} bottom={20}>
+            <Controller left={20} bottom={60}>
                 <Button content={<Play />} tooltip='Play' onClick={() => console.log('play')} />
                 <Button content={<CaretLeft  />} tooltip='fast' onClick={() => console.log('play')} />
                 <Button content={'1x'} tooltip='Play' onClick={() => console.log('play')} />
