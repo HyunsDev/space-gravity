916792f06eb779ae205eafcaab13905c6633e4fd
혀느현스 <hyuns@hyuns.dev>
2022-05-04 04:25:31 +0900

    중간 커밋

--- public/simulator.js
+++ public/simulator.js
@@ -1,102 +1,93 @@
 /* eslint-disable no-restricted-globals */
 
 
+let loopId
+let planets = {}
+let speed = 1
+const SPEED_RADIUS = 200
+const SPACE_G = 0.005
 
-function simulator() {
-    let loopId
-    let planets= {}
-    let speed = 1
-    const SPEED_RADIUS = 200
-    const SPACE_G = 1
-
-    const getSquaredDistance = (planet, targetPlanet) => {
-        return Math.round((planet.x - targetPlanet.x)**2 + (planet.y - targetPlanet.y)**2)
-    }
-
-    // const getVector = ( a1, a2 ) => {
-
-    // }
-
-
-    const getGravitationalAcceleration = (planet, targetPlanet) => {
-        return {
-            ax: - SPACE_G * planet.weight * targetPlanet.weight * ( planet.x - targetPlanet.x ) / Math.abs((planet.x - targetPlanet.x))**3,
-            ay: - SPACE_G * planet.weight * targetPlanet.weight * ( planet.y - targetPlanet.y ) / Math.abs((planet.y - targetPlanet.y))**3
-        }
-    }
-
-    loopId = setTimeout(function loop() {
-        const newPlanets = {...planets}
-
-        // 주요 로직
-        for (let planetId of Object.keys(planets)) {
-            const planet = newPlanets[planetId]
-            if ( !planet ) continue // 삭제된 행성 계산 무시
-
-            newPlanets[planetId] =  {
-                weight: planet.weight,
-                radius: planet.radius,
-                x: planet.x + planet.vx / SPEED_RADIUS,
-                y: planet.y + planet.vy / SPEED_RADIUS,
-                vx: planet.vx,
-                vy: planet.vy,
-                color: planet.color
-            }
-
-            for (let [targetPlanetId, targetPlanet] of Object.entries(newPlanets)) { // 충돌 감지 & 중력
-                if (targetPlanetId === planetId) continue
-
-                // 충돌 감지
-                if (getSquaredDistance(planet, targetPlanet) < (planet.radius + targetPlanet.radius)**2) {
-                    const weight = newPlanets[planetId].weight + newPlanets[targetPlanetId].weight
-                    const radius = Math.round(Math.sqrt(newPlanets[planetId].radius**2 + newPlanets[targetPlanetId].radius**2))
-
-                    if (planet.radius >= targetPlanet.radius) {
-                        newPlanets[planetId].weight = weight
-                        newPlanets[planetId].radius = radius
-                        delete newPlanets[targetPlanetId]
-                    } else {
-                        newPlanets[targetPlanetId].weight = weight
-                        newPlanets[targetPlanetId].radius = radius
-                        delete newPlanets[planetId]
-                    }
-
-                    continue
-                }
-
-                // 중력 가속도
-                const a = getGravitationalAcceleration(planet, targetPlanet)
-                newPlanets[planetId].vx = newPlanets[planetId].vx + a.ax
-                newPlanets[planetId].vy = newPlanets[planetId].vy + a.ay
-            }
-        }
-
-        planets = newPlanets
-        self.postMessage({kind: 'newPlanets', planets: newPlanets})
-        setTimeout(loop, 128/speed)
-    }, 128/speed)
-
-    // IO
-    self.addEventListener('message', event => {
-        switch (event.data.kind) {
-            case 'planetAdd':
-                // self.postMessage(event.data.planets)
-                console.log(event.data.newPlanet.data)
-                planets[event.data.newPlanet.id] = event.data.newPlanet.data
-                break
-
-            case 'speedUpdate':
-                speed = event.data.speed;
-                break
-
-            case 'stopSimulate':
-                clearInterval(loopId)
-                break
-
-            default:
-                console.error('Wrong Command')
-        }
-    })
+const getSquaredDistance = (planet, targetPlanet) => {
+    return Math.round((planet.x - targetPlanet.x)**2 + (planet.y - targetPlanet.y)**2)
 }
 
-simulator()
+const getGravitationalAcceleration = (planet, targetPlanet) => {
+    return {
+        ax: - SPACE_G * planet.weight * targetPlanet.weight * ( planet.x - targetPlanet.x ) / Math.abs((planet.x - targetPlanet.x))**3,
+        ay: - SPACE_G * planet.weight * targetPlanet.weight * ( planet.y - targetPlanet.y ) / Math.abs((planet.y - targetPlanet.y))**3
+    }
+}
+
+loopId = setTimeout(function loop() {
+    const newPlanets = {...planets}
+
+    // 주요 로직
+    for (let planetId of Object.keys(planets)) {
+        const planet = newPlanets[planetId]
+        if ( !planet ) continue // 삭제된 행성 계산 무시
+
+        newPlanets[planetId] =  {
+            weight: planet.weight,
+            radius: planet.radius,
+            x: planet.x + planet.vx / SPEED_RADIUS,
+            y: planet.y + planet.vy / SPEED_RADIUS,
+            vx: planet.vx,
+            vy: planet.vy,
+            color: planet.color
+        }
+
+        for (let [targetPlanetId, targetPlanet] of Object.entries(newPlanets)) { // 충돌 감지 & 중력
+            if (targetPlanetId === planetId) continue
+
+            // 충돌 감지
+            if (getSquaredDistance(planet, targetPlanet) < (planet.radius + targetPlanet.radius)**2) {
+                const weight = newPlanets[planetId].weight + newPlanets[targetPlanetId].weight
+                const radius = Math.round(Math.sqrt(newPlanets[planetId].radius**2 + newPlanets[targetPlanetId].radius**2))
+
+                if (planet.weight >= targetPlanet.weight) {
+                    newPlanets[planetId].weight = weight
+                    newPlanets[planetId].radius = radius
+                    delete newPlanets[targetPlanetId]
+                } else {
+                    newPlanets[targetPlanetId].weight = weight
+                    newPlanets[targetPlanetId].radius = radius
+                    delete newPlanets[planetId]
+                }
+                continue
+            }
+
+            // 중력 가속도
+            const a = getGravitationalAcceleration(planet, targetPlanet)
+            newPlanets[planetId].vx = newPlanets[planetId].vx + a.ax
+            newPlanets[planetId].vy = newPlanets[planetId].vy + a.ay
+        }
+    }
+
+    planets = newPlanets
+    self.postMessage({kind: 'newPlanets', planets: newPlanets})
+    setTimeout(loop, 128/speed)
+}, 1024/speed)
+
+
+
+// IO
+self.addEventListener('message', event => {
+    switch (event.data.kind) {
+        case 'planetAdd':
+            // self.postMessage(event.data.planets)
+            console.log(event.data.newPlanet.data)
+            planets[event.data.newPlanet.id] = event.data.newPlanet.data
+            break
+
+        case 'speedUpdate':
+            speed = event.data.speed;
+            break
+
+        case 'stopSimulate':
+            clearInterval(loopId)
+            break
+
+        default:
+            console.error('Wrong Command')
+    }
+})
---
+++ src/context/newPlant.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from "react";
+
+export const NewPlanetContext = createContext({
+    isCreating: false,
+})
+
+const NewPlanetProvider: React.FC<React.ReactNode> = ({children}) => {
+    const [isCreating, setCreating] = useState(false)
+    const [speed, setSpeed] = useState(3)
+    const [radius, setRadius] = useState(16)
+    const [weight, setWeight] = useState(32)
+
+    return (
+        <NewPlanetContext.Provider
+            value={{
+                isCreating,
+                
+                
+            }}
+        >
+            {children}
+        </NewPlanetContext.Provider>
+    )
+}
+
+export default NewPlanetProvider;
